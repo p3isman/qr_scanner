5b2fcc8af95c23e9fe03fb3802e178b52af08b1a
RealMagus <pedro_eisman@hotmail.com>
2021-05-05 19:15:35 +0200

    update map_page

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,18 +1,38 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qr_scanner/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  final CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     final ScanModel scan =
         ModalRoute.of(context)!.settings.arguments as ScanModel;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Coordenadas'),
-        ),
-        body: Center(
-          child: Text(scan.value),
-        ));
+      appBar: AppBar(
+        title: Text('Coordenadas'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
   }
 }
