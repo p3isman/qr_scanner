96457d641efc9460c36a59cfe3becb2ab5de95c4
p3isman <pedro_eisman@outlook.com>
2023-02-21 16:38:18 +0100

    Use newer methods to launch URLs

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:qr_scanner/models/scan_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// Launches a URL in browser
 void launchURL(BuildContext context, ScanModel scan) async {
-  final url = scan.value;
+  final url = Uri.parse(scan.value);
 
   if (scan.type == 'http') {
-    await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
+    await canLaunchUrl(url)
+        ? await launchUrl(url)
+        : throw 'Could not launch $url';
   } else {
     Navigator.pushNamed(context, 'map', arguments: scan);
   }
