026d47ea53ea2360885aa5a4797e99904cf90ed0
p3isman <pedro_eisman@outlook.com>
2023-02-21 17:51:48 +0100

    Remove comments

--- lib/providers/ui_provider.dart
+++ lib/providers/ui_provider.dart
@@ -3,16 +3,12 @@ import 'package:flutter/material.dart';
 class UiProvider extends ChangeNotifier {
   int _selectedMenuOpt = 0;
 
-  // Getter uses {}
   int get selectedMenuOpt {
     return _selectedMenuOpt;
   }
 
-  // Setter acts like a function
   set selectedMenuOpt(int i) {
     _selectedMenuOpt = i;
-
-    // Triggers changes to widgets that are listening to this
     notifyListeners();
   }
 }
