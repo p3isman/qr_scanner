4f4c4c54d5b860aa114a9a93ac91377d6884397e
p3isman <pedro_eisman@outlook.com>
2023-02-21 11:14:02 +0100

    Rename unused parameter

--- lib/widgets/list_tiles.dart
+++ lib/widgets/list_tiles.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:provider/provider.dart';
-
 import 'package:qr_scanner/providers/scan_list_provider.dart';
 import 'package:qr_scanner/utils/utils.dart';
 
@@ -26,7 +24,7 @@ class ListTiles extends StatelessWidget {
           return Dismissible(
             key: UniqueKey(),
             background: Container(color: Colors.red),
-            onDismissed: (DismissDirection direction) {
+            onDismissed: (_) {
               Provider.of<ScanListProvider>(context, listen: false)
                   .deleteScanById(scans[i].id!);
             },
